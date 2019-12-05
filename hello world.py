bada51b4450d76361c686d459080e3b8c7640d55
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2019-12-05 15:36:50 +1300

    create script

---
+++ hello world.py
@@ -0,0 +1,16 @@
+# this program says Hello and asks for my name
+
+print("Hello World")
+print("What is your name?") # asks for their name
+Myname = input()
+
+print("It is good to meet you " + Myname)
+print("The lenght of your name is:")
+print(len(Myname))
+
+print("What is your age")
+Myage = input()
+
+print("You will be " + str(int(Myage) + 1) + " In a Year")
+
+
